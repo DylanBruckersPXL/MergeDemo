10295a52a885009c4bc4607f5896d0ef59ee9d66
Djordy Nelissen <djordynelissen@gmail.com>
2021-10-29 09:22:01 +0200

    Added button with merge conflict text

--- MergeDEMO/MergeDEMO/MainWindow.xaml.cs
+++ MergeDEMO/MergeDEMO/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MergeDEMO
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Dit wordt een merge conflict");
+        }
     }
 }
